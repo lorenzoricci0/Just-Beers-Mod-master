eb8621af30a2b88d6eccd217ac9ec85c44ecd597
lorenzoricci0 <160848911+lorenzoricci0@users.noreply.github.com>
2024-03-15 19:28:15 +0100

    +

--- src/main/java/net/carlo/justbeersmod/item/ModItems.java
+++ src/main/java/net/carlo/justbeersmod/item/ModItems.java
@@ -12,23 +12,32 @@ public class ModItems {
     public static final Item MUG = registerItem("mug",
             new Item(new FabricItemSettings().group(ModGroup.JUST_BEERS).maxCount(8)));
     public static final Item WHEAT_BEER = registerItem("wheat_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.WHEAT_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.WHEAT_BEER).maxCount(8)));
     public static final Item STRONG_BEER = registerItem("strong_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.STRONG_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.STRONG_BEER).maxCount(8)));
     public static final Item SWEET_BEER = registerItem("sweet_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.SWEET_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.SWEET_BEER).maxCount(8)));
     public static final Item MINER_BEER = registerItem("miner_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.MINER_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.MINER_BEER).maxCount(8)));
     public static final Item BLAZING_BEER = registerItem("blazing_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.BLAZING_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.BLAZING_BEER).maxCount(8)));
     public static final Item FROST_BEER = registerItem("frost_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.FROST_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.FROST_BEER).maxCount(8)));
     public static final Item APPLE_BEER = registerItem("apple_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.APPLE_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.APPLE_BEER).maxCount(8)));
     public static final Item PUMPKIN_BEER = registerItem("pumpkin_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.PUMPKIN_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.PUMPKIN_BEER).maxCount(8)));
     public static final Item BERRY_BEER = registerItem("berry_beer",
-            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).food(ModFoodComponents.BERRY_BEER).maxCount(8)));
+            new BeerItem(new FabricItemSettings().group(ModGroup.JUST_BEERS).
+                    food(ModFoodComponents.BERRY_BEER).maxCount(8)));
 
     public static final Item WHEAT_AND_SUGAR_COMPOUND = registerItem("wheat_and_sugar_compound",
             new Item(new FabricItemSettings().group(ModGroup.JUST_BEERS)));
